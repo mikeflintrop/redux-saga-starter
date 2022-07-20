0593157669159a77c743a5d96660b82148c4e491
Mike Flintrop <mike.flintrop@gmail.com>
2022-07-20 09:51:27 -0500

    notes1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,73 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+function* myGenerator() {
+    try {
+        yield true;
+        yield 100;
+        yield 'helloooo';
+        yield [1, 2, 3];
+        yield {key: value};
+    }
+    catch (error) {
+        console.log('Error in myGenerator', error);
+    };
+};
+
+// create instance of generator function
+const goDogGo = myGenerator();
+
+// call generator function with .next() method
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+
+function* getSwitch() {
+    try {
+        while(true) {
+            yield 'on';
+            yield 'off';
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+};
+
+const toggle = getSwitch();
+
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+
+function* countDownGenerator() {
+    let a = 10;
+    while (a > 0) {
+        yield `launching in ${a}`;
+        a -= 1;
+    }
+    yield 'Take off';
+}
+
+const countDown = countDownGenerator();
+
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+console.log(countDown.next().value)
+
 const elementList = (state = [], action) => {
     switch (action.type) {
         case 'SET_ELEMENTS':
@@ -14,7 +81,7 @@ const elementList = (state = [], action) => {
         default:
             return state;
     }
-};    
+};
 
 // this is the saga that will watch for actions
 function* watcherSaga() {
